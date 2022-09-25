62febdebd8c52847dda1263dd963aad87c149c30
Tim Bhison <catbat04@gmail.com>
2022-09-25 18:16:26 +0100

    types

---
+++ types.ts
@@ -0,0 +1,10 @@
+export type bigStartingNumber = 25 | 50 | 75 | 100;
+export type smallStartingNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 9 | 10;
+export type startingNumber = bigStartingNumber | smallStartingNumber;
+export type operator = "multiply" | "divide" | "add" | "subtract";
+
+export type sum = {
+  leftNumber?: number;
+  rightNumber?: number;
+  operator?: operator;
+};
